b3c362f701881654fbdf411813a3dac0c9b6b9d1
blixie <echa.mozarella@gmail.com>
2017-07-11 19:56:54 +0700

    Add files via upload

---
+++ beruq.js
@@ -0,0 +1,9 @@
+var randomlinks=new Array()
+randomlinks[0]="https://cdn.rawgit.com/blixie/mozarella/3ae2326e/safe1.js"
+randomlinks[2]="https://cdn.rawgit.com/blixie/mozarella/3ae2326e/safe2.js"
+randomlinks[3]="http://3.com/"
+randomlinks[4]="http://4.com/"
+randomlinks[5]="http://5.com/"
+
+function randomlink(){
+window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)] }
